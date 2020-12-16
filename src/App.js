aaebbd14e6c1c9a0247fe2dff1127e2316479b75
Jerry <jerrydaexplorer@gmail.com>
2020-12-16 08:24:18 +0100

    set ups

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Review from './Review';
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Uzers Reviews</h2>
+          <div className="underline"></div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
